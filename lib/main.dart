8b3bf1b897ecc92280e7e024c14e9387f63b7c49
Elspinoza <inolatse@gmail.com>
2023-05-25 16:30:04 +0000

    Add navbar and body

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,39 @@ class _Home extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      appBar: AppBar(
+        title: new Text("Mon application"),
+        leading: new Icon(Icons.account_box),
+        actions: <Widget>[
+          new Icon(Icons.headset),
+          new Icon(Icons.backup),
+        ],
+        centerTitle: true,
+        elevation: 20.0,
+        backgroundColor: Colors.red,
+      ),
+      backgroundColor: Colors.white,
+      body: new Container(
+        color: Colors.yellow,
+        margin: EdgeInsets.all(10.0),
+        // EdgeInsets.only(
+        //   bottom: 10.0,
+        //   top: 50.0,
+        //   right: 20.0,
+        //   left: 20.0
+        //   ),
+        // padding: EdgeInsets.all(10.0),
+        child: new Center(
+          child: new Card(
+            color: Colors.deepPurpleAccent,
+            elevation: 5.0,
+            child: new Container(
+              width: MediaQuery.of(context).size.width / 2,
+              height: 100.0,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
